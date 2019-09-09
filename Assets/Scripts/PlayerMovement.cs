73ff4cee239532dfc46c77731e90b9a31b6f19cb
Damian Metz <drmetz11@hotmail.com>
2019-09-09 15:44:09 -0400

    Added PlayerMovementController

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public bool isOnGround = false;
+    public float jumpPower = 7.0f;
+
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent(typeof(Transform)) as Transform;
+        _rigidbody = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        Jump();
+    }
+
+    void MovePlayer()
+    {
+        float translate = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        _transform.Translate(translate, 0, 0);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isOnGround = true;
+    }
+
+    void OnCollisionExit2D()
+    {
+        isOnGround = false;
+    }
+}
