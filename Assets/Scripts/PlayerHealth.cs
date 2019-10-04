64ae76bb1a8140e60ac0dc2fa3ad5b0bcea0d02c
bpeter340 <bartpeter@outlook.com>
2019-10-04 18:31:43 -0400

    Player can die and respawn

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    GameObject player;
+    public int health;
+    public bool hasDied;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        hasDied = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if(player.transform.position.y < -4)
+        {
+            hasDied = true;
+        }
+        if (hasDied)
+        {
+            StartCoroutine("Die"); 
+        }
+    }
+
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("SampleScene");
+        yield return null;
+    }
+}
