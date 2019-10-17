ae6772133db2a76a61cfe3c409958d55db8b9bf0
bpeter340 <bartpeter@outlook.com>
2019-10-17 13:52:11 -0400

    minor changes

---
+++ Assets/Scripts/EnemyWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    public GameObject bullet;
+    private PlayerMovement playerMovement;
+    public float timer = 0;
+    private Vector3 position = new Vector3(5,5,0);
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        updateTime();
+
+        if(timer >= 2)
+        {
+            timer = 0;
+            //This is very buggy
+            var thisBullet = Instantiate(bullet, position, bullet.transform.rotation) as GameObject;
+            thisBullet.GetComponent<Bullet>().bulletDirection = playerMovement.PlayerDirection;
+        }
+    }
+
+    void updateTime()
+    {
+        timer += Time.deltaTime;
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,31 +5,22 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public GameObject bullet;
-
     private PlayerMovement playerMovement; // for bullet direction
-
     private AudioPlayerWrapper _audioPlayer;
-
-    // AudioClip
     public AudioClip pewpew;
     public AudioClip bigPew;
 
     public float chargeTimer = 0;
-
     private float chargeRate = 2f;
     public float growthRate = .1f;
-
     private bool buttonHeldDown = false;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
         _audioPlayer = GetComponent(typeof(AudioPlayerWrapper)) as AudioPlayerWrapper;
     }
 
-    // Update is called once per frame
     void Update()
     {
   
@@ -41,7 +32,6 @@ public class Weapon : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             HoldButton();
-            //chargeTimer += Time.deltaTime;
         }
         else if (Input.GetButtonUp("Fire1"))
         {
@@ -50,62 +40,28 @@ public class Weapon : MonoBehaviour
 
             float scaleVal;
             int addedDamage = 0;
+
             if (chargeTimer < 2)
             {
                 scaleVal = growthRate; // scale bullet size
-                // +2 damage if held for over 1 sec
                 if (chargeTimer > 1)
                     addedDamage += 2;
 
                 _audioPlayer.PlaySound(pewpew);
             }
-            else // chargeTimer >= 2
+            else
             {
                 // max bullet size and damage
                 scaleVal = .26f;
                 addedDamage = 5;
-                // bullet becomes green
                 cloneBullet.GetComponent<Renderer>().material.color = Color.green;
-
                 _audioPlayer.PlaySound(bigPew);
             }
 
             cloneBullet.transform.localScale += new Vector3(scaleVal, scaleVal, scaleVal);
             cloneBullet.GetComponent<Bullet>().damage += addedDamage;
             ButtonReleased();
-        }
-        /* old
-        if (Input.GetButtonUp("Fire1") && chargeTimer > 2)
-        {
-            var cloneBullet = Instantiate(bullet, gameObject.transform.position, bullet.transform.rotation) as GameObject;
-            cloneBullet.GetComponent<Bullet>().bulletDirection = playerMovement.PlayerDirection;
-            _audioPlayer.PlaySound(pewpew);
-            //Set max bullet size
-            cloneBullet.transform.localScale += new Vector3(.26f, .26f, .26f);
-            //Set max bullet power 
-            cloneBullet.GetComponent<Bullet>().damage = 10;
-            //Change bullet color
-            cloneBullet.GetComponent<Renderer>().material.color = Color.green;
-            ButtonReleased();
-        }
-        else if (Input.GetButtonUp("Fire1") && chargeTimer < 2)
-        {
-            var val = growthRate;
-
-            var cloneBullet = Instantiate(bullet, gameObject.transform.position, bullet.transform.rotation) as GameObject;
-            cloneBullet.GetComponent<Bullet>().bulletDirection = playerMovement.PlayerDirection;
-            _audioPlayer.PlaySound(pewpew);
-            //Scale bullet size
-            cloneBullet.transform.localScale += new Vector3(val, val, val);
-
-            //increments bullet damage by 2 after 1 second
-            if(chargeTimer > 1)
-                cloneBullet.GetComponent<Bullet>().damage += 2;
-
-            ButtonReleased();
-         
-        }
-        */
+        } 
     }
 
     public void HoldButton()
