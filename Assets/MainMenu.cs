80c9fe7362f5b50f62541ff1257ad8c3fe619ed5
Dsnow21 <dfsnow01@indianatech.net>
2019-11-25 12:08:13 -0500

    Main Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("First Level Design");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Lava Level");
+    }
+
+}
