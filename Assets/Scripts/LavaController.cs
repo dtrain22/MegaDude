d9cb40bac5625bb2448c397af1bb293288024486
Dsnow21 <DFSnow01@indianatech.net>
2019-12-02 11:34:09 -0500

    Lava Controller

--- Assets/Scripts/LavaController.cs
+++ Assets/Scripts/LavaController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LavaController : MonoBehaviour
 {
+    public GameObject player;
+    public float currentHeight;
+    public float maxHeight;
+    private Vector3 move;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHeight = transform.position.y;
+        maxHeight = 37;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHeight < maxHeight && player.transform.position.x > 197)
+        {
+            float newY = transform.position.y + .05f;
+            move.Set(transform.position.x, newY, transform.position.z);
+            transform.position = move;
+        }
     }
 }
