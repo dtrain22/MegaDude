451d97a9c28e045988f60439f1d2758e3d2b1b58
Salemfh <34492681+Salemfh@users.noreply.github.com>
2019-12-04 12:54:37 -0500

    Sound changes
    Added the sounds to the game.

---
+++ Assets/Scripts/AudioPlayerWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayerWrapper : MonoBehaviour
+{
+    public AudioSource _audioSource;
+
+    void Start()
+    {
+        // find the player audio source
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip, float volumeScale = 1.0f)
+    {
+        _audioSource.PlayOneShot(clip, volumeScale);
+    }
+}
